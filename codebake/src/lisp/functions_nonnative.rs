20e3d4cbc9a8671fda76b1aa9ec3360cd97e2617
Matt Wyatt <mwyatt1000@gmail.com>
2022-07-10 06:30:57 -0400

    fix map & reduce not using new empty fxn

--- codebake/src/lisp/functions_nonnative.rs
+++ codebake/src/lisp/functions_nonnative.rs
@@ -7,14 +7,14 @@ pub static FUNCTIONS_NONNATIVE: &[&'static str] = &[LISP_MAP, LISP_REDUCE];
 
 static LISP_MAP: &'static str = "
 (defn map (f lis)
-  (if (empty lis)
+  (if (empty? lis)
     (quote ())
     (cons (f (first lis)) (map f (rest lis)))))
 ";
 
 static LISP_REDUCE: &'static str = "
 (defn reduce (f acc lis)
-  (if (empty lis)
+  (if (empty? lis)
     acc
     (f (first lis) (reduce f acc (rest lis)))))
 ";
