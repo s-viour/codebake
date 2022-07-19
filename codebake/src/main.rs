14767394b41e71d5075104911c2df17efcd9122d
Bee <mrhumanman2@gmail.com>
2022-07-19 20:55:25 +0000

    Unhijack main (hehe)

--- codebake/src/main.rs
+++ codebake/src/main.rs
@@ -1,19 +1,4 @@
 use codebake::lisp;
-
-use regex::Regex;
-
 fn main() {
-    // lisp::run_repl(None);
-    
-    let re = Regex::new(".").unwrap();
-    let v = Vec::new();
-    
-    for m in re.find_iter("nice") {
-        let e = m.as_str().to_string();
-    
-        v.push(&e);
-        println!("{}", e)
-    }
-    
-    println!("{:#?}", v.join("\n"))
+    lisp::run_repl(None);
 }
